031d03f29a105860f89f35f137b0fde488d36413
Mikhail Shembelev <89518906+MikhailSembelev@users.noreply.github.com>
2024-01-05 01:22:07 +0300

    init project

---
+++ src/main/java/com/mshembelev/mindskeeper/MindskeeperApplication.java
@@ -0,0 +1,13 @@
+package com.mshembelev.mindskeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindskeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindskeeperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mshembelev/mindskeeper/MindskeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mshembelev.mindskeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindskeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
